234ab35695603de347b338031d72078ed051aac3
itroy32tq <139143911+itroy32tq@users.noreply.github.com>
2023-07-31 14:08:00 +0300

    commit
    изучил и разобрал решение, к сожалению мое старое решение под данный обсервер не подходит, и в целом все выглядит довольно странно, это не некое общее решение для подобных случаев

---
+++ Assets/Scrypt/Observer/Enum.cs
@@ -0,0 +1,15 @@
+namespace Checkers
+{
+    public enum MoveType
+    {
+        Highlight,
+        Move,
+        End
+    }
+    public enum RecordType
+    {
+        Move,
+        Remove,
+        Click
+    }
+}
